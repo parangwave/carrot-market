60114e36435c5db717d07440d165e13d1089c4e6
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-01 00:34:16 +0900

    Tailwind - List & Animations
    odd, even, first, last를 활용해 element의 순서에 따라 style 제어함
    animation(animate-...)를 통해 animation을 제어함
    empty-[state]를 통해 빈 값의 element의 style을 제어함

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,37 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-100 md:bg-orange-100 lg:bg-green-100 xl:bg-cyan-100 2xl:bg-purple-100 dark:bg-gray-700">
-      <div className="flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg ring ring-transparent transition-shadow *:outline-none has-[.peer]:bg-green-100 has-[:invalid]:ring has-[:invalid]:ring-red-100 md:flex-row dark:bg-gray-600">
-        <input
-          type="email"
-          placeholder="Email Address"
-          required
-          className="peer h-12 w-full rounded-full bg-gray-200 py-3 pl-5 ring ring-transparent transition-shadow placeholder:font-medium placeholder:drop-shadow invalid:bg-red-100 focus:ring-green-500 focus:ring-offset-2 invalid:focus:ring-red-500"
-        />
-        <span className="hidden font-medium text-red-500 peer-invalid:block">
-          Email is required
-        </span>
-        <button className="rounded-full bg-black py-2 font-medium text-white outline-none transition-transform active:scale-90 md:px-10">
-          Login
-        </button>
+      <div className="flex w-full max-w-screen-sm flex-col gap-3 rounded-3xl bg-white p-5 shadow-lg dark:bg-gray-600">
+        {["nico", "me", "you", "other", ""].map((person, index) => (
+          // // odd:, even:
+          // <div
+          //   key={index}
+          //   className="flex items-center gap-5 rounded-xl p-2.5 odd:bg-gray-100 even:bg-cyan-100"
+          // >
+
+          // // first:, last:
+          // <div
+          //   key={index}
+          //   className="flex items-center gap-5 border-b-2 p-2.5 pb-5 last:border-0 last:pb-0"
+          // >
+
+          // <div key={index} className="flex items-center gap-5 *:animate-pulse">
+          <div key={index} className="flex items-center gap-5">
+            {/* skeleton ui */}
+            {/* <div className="size-10 rounded-full bg-blue-400" />
+            <div className="h-4 w-40 rounded-full bg-gray-400" />
+            <div className="h-4 w-20 rounded-full bg-gray-400" /> */}
+            <div className="size-10 rounded-full bg-blue-400" />
+            <span className="text-lg font-medium empty:h-5 empty:w-24 empty:animate-pulse empty:rounded-full empty:bg-gray-300">
+              {person}
+            </span>
+            <div className="relative flex size-5 items-center justify-center rounded-full bg-red-500 text-white">
+              <span className="z-10">{index}</span>
+              <div className="absolute size-6 animate-ping rounded-full bg-red-500" />
+            </div>
+            {/* <span className="animate-spin">⌛</span> */}
+          </div>
+        ))}
       </div>
     </main>
   )
