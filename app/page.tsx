a0b2358895797ed1f41dffe5c378da836277de31
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-01 22:09:41 +0900

    Tailwind - Directives
    directive는 place-holder같은 것.
    따라서 컴파일러는 base를 통해 모든 base style을 넣고, utilities를 통해 모든 utilities style을 넣음
    components를 통해 많은 클래스를 감싸서 style을 재사용할 수 있음

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
+    <main className="flex h-screen items-center justify-center bg-red-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
       <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg">
         <div className="group flex flex-col">
-          <input
-            className="w-full rounded-full bg-gray-100 p-2"
-            placeholder="Write your email"
-          />
-          {/* group-focus-within: group container의 어떤 자식이 focus되었는지 확인함 */}
-          <span className="hidden group-focus-within:block">
-            Make sure it is a valid email...
-          </span>
-          <button className="rounded-sexy-name mt-tomato h-10 w-full bg-black text-white">
-            Submit
-          </button>
+          <input />
+          <h1 className="text-bigger-hello">hello world</h1>
+          <a href="#">aaaa</a>
+          <button className="btn">Submit</button>
         </div>
       </div>
     </main>
