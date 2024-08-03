73a28ed9a206cbaa376e1efe7b19a71e632817b0
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-04 01:27:46 +0900

    Server Actions - useFormState
    server action 결과를 ui로 전달하는 방법 = useFormState()을 사용하여 [action의 결과, action을 실행하는 트리거]를 받아 ui에 제공함

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,22 @@
+"use client"
+
 // components
 // w/ import alias "@/"
 import FormInput from "@/components/form-input"
 import FormButton from "@/components/form-btn"
 import SocialLogin from "@/components/social-login"
+import { handleForm } from "./actions"
+import { useFormState } from "react-dom"
 
 export default function Login() {
-  const handleForm = async (formData: FormData) => {
-    // nextjs create route-handler for POST method automatically
-    // auto sending data for BE
-    // need name in input to get data from form (network>payload)
-    "use server"
-    // 사용자가 계속 클릭해서 race condition에 들어가는 것을 방지함
-    // 1. 사용자에게 이 server action이 시간이 좀 걸린다는 것을 알려줌
-    // 2. 버튼을 비활성화
-    await new Promise((resolve) => setTimeout(resolve, 5000))
-    console.log("logged in!")
-  }
+  // useActionState: [action의 결과, action을 실행하는 트리거]를 제공함
+  // action을 호출 -> formData & 이전에 반환한 state (or 초기에 설정한 state)와 같이 실행
+  // =>  handleForm을 실행 & action의 return값을 state로
+  // 원래 error:string을 return
+  // const [state, action] = useFormState(handleForm, {
+  //   potato: 1,
+  // } as any)
+  const [state, action] = useFormState(handleForm, null)
 
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
@@ -23,7 +24,7 @@ export default function Login() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Login with email & password</h2>
       </div>
-      <form action={handleForm} className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="email"
           type="email"
@@ -36,7 +37,7 @@ export default function Login() {
           type="password"
           placeholder="Confirm Password"
           required
-          errors={[]}
+          errors={state?.errors ?? []}
         />
         <FormButton text="Log in" />
       </form>
