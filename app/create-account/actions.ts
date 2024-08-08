8273a528be77c53e8189c15f86e59e6e27d34c97
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-08 13:44:25 +0900

    Validation - Validation Errors
    zod가 유효성 검사 로직을 자동적으로 제공하고 ts가 자동적으로 인식함.
    flatten()을 통해 error를 더 잘 관리할 수 있음.

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -4,7 +4,14 @@ import { z } from "zod"
 
 // zod에게 data type, limit을 설명하는 schema 제공
 // len == min 5 ~ max 10
-const usernameSchema = z.string().min(5).max(10)
+// schema에서 데이터 유효성 검사를 하고 에러를 발생 시킴
+// 에러 메세지에는 form item들의 개별적인 에러가 들어가 있음
+const formatSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(10),
+  confirm_password: z.string().min(10),
+})
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -13,5 +20,12 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   }
-  usernameSchema.parse(data.username)
+  // parse() throws error -> try-catch
+  // safeParse() X throws error
+  const result = formatSchema.safeParse(data)
+  if (!result.success) {
+    // flatten()은 축소된 error obj
+    // unless flatten(), big error obj
+    return result.error.flatten() // to form state -> ui
+  }
 }
--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -10,6 +10,7 @@ import { createAccount } from "./actions"
 import { useFormState } from "react-dom"
 
 export default function CreateAccount() {
+  // ts auto knows [ createAccout action's return value === zod's flattenError type ]
   const [state, dispatch] = useFormState(createAccount, null)
 
   return (
@@ -24,19 +25,29 @@ export default function CreateAccount() {
           type="text"
           placeholder="Username"
           required
+          // errors = string arr || null
+          errors={state?.fieldErrors.username}
         />
-        <FormInput name="email" type="email" placeholder="Email" required />
+        <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors.email}
+        />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
+          errors={state?.fieldErrors.password}
         />
         <FormInput
           name="confirm_password"
           type="password"
           placeholder="Confirm Password"
           required
+          errors={state?.fieldErrors.confirm_password}
         />
         <FormButton text="Create Account" />
       </form>
