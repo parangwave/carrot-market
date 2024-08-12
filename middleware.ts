14f7495ba8931fa8ae82e05c54b4ad3410d19a21
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-13 01:38:14 +0900

    Authentication - Authentication Middleware
    user가 로그인했는지 확인하고 url 접근을 제한하고 redirect함

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,19 @@
 // image 가져올 때, js코드를 다운로드할 때, 브라우저가 css, js 파일 등을 다운받을 때
 // google의 어떤 로봇이 웹 사이트를 볼 때, favicon을 가져와야 할 때
 import { NextRequest, NextResponse } from "next/server"
+import getSession from "./lib/session"
+
+interface Routes {
+  [key: string]: boolean
+}
+
+//   public으로 접근 가능한 url obj === 인증되지 않은 user가 갈 수 있는 url 저장
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+}
 
 // middleware를 특정 페이지에서만 실행되도록 하는 법
 // 즉, 특정 request에서 실행되지 않도록
@@ -19,18 +32,27 @@ import { NextRequest, NextResponse } from "next/server"
 // middleware가 모든 단일 request에 대해 실행되어야 함
 // npm packages === a smaller subset, prisma를 사용할 수 없는 이유
 export async function middleware(request: NextRequest) {
-  console.log("hello")
+  const session = await getSession()
+  // user가 가려는 pathname을 확인함
+  // 해당 pathname이, 즉 그 page가 이 obj에 존재하는 지 확인함
+  // 그 결과를 exists에 Boolean값으로 저장
+  // user가 이동하려는 url이나 pathname은 이 obj에 없음
+  // 즉, 여기에 없는 페이지로 이동한다는 뜻 === user가 log out 상태
+  const exists = publicOnlyUrls[request.nextUrl.pathname]
 
-  //   const pathname = request.nextUrl.pathname
-  //   if (pathname === "/") {
-  //     // user에게 실제로 제공할 response를 가져옴
-  //     const response = NextResponse.next()
-  //     response.cookies.set("middleware-cookie", "hello!")
-  //     return response
-  //   }
-  //   if (pathname === "/profile") {
-  //     return NextResponse.redirect(new URL("/", request.url))
-  //   }
+  //   user가 session & cookie랑 id를 가지고 있지 않다면
+  if (!session.id) {
+    if (!exists) {
+      // user를 redirect하는 response를 return
+      return NextResponse.redirect(new URL("/", request.url))
+    }
+  } else {
+    // user가 로그인한 상태 && publicOnlyUrls로 간다면,
+    // 허용 X && /products로 보낼 것
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url))
+    }
+  }
 }
 
 // cookie 설정하는 법
