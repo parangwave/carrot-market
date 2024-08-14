5ce8f0028adbcc84432559ea1a9ce828666ef90b
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-14 22:21:19 +0900

    Social Authentication - Twilio SMS
    Twilio을 통해 phone으로 코드 전송

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import twilio from "twilio"
 import crypto from "crypto"
 import { z } from "zod"
 import validator from "validator" // string 유효성 검사 라이브러리
@@ -112,6 +113,17 @@ export async function smsLogIn(prevState: ActionState, formData: FormData) {
       })
 
       // send the token using twilio
+      const client = twilio(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_AUTH_TOKEN
+      )
+
+      await client.messages.create({
+        body: `Your Karrot verification code is: ${token}`,
+        from: process.env.TWILIO_PHONE_NUMBER!,
+        to: process.env.MY_PHONE_NUMBER!,
+      })
+
       return {
         token: true,
       }
