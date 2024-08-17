4ec3d91c63989de15471853748f89a792a8e540d
Oh Jisu <parangwavetraveller@gmail.com>
2024-08-17 23:59:54 +0900

    Product Upload - Intro

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,6 +1,8 @@
 import ProductList from "@/components/product-list"
 import db from "@/lib/db"
+import { PlusIcon } from "@heroicons/react/24/solid"
 import { Prisma } from "@prisma/client"
+import Link from "next/link"
 
 async function getInitialProducts() {
   const products = await db.product.findMany({
@@ -29,6 +31,12 @@ export default async function Products() {
   return (
     <div className="flex flex-col gap-5 p-5">
       <ProductList initialProducts={initialProducts} />
+      <Link
+        href="/products/add"
+        className="fixed bottom-24 right-8 flex size-16 items-center justify-center rounded-full bg-orange-500 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
     </div>
   )
 }
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -73,10 +73,7 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {!isLastPage ? (
         <span
           ref={trigger}
-          style={{
-            marginTop: `${page + 1 * 900}vh`,
-          }}
-          className="mx-auto mb-96 w-fit rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold hover:opacity-90 active:scale-95"
+          className="mx-auto w-fit rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold hover:opacity-90 active:scale-95"
         >
           {isLoading ? "로딩 중" : "Load more"}
         </span>
